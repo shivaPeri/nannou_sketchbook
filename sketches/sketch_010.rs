1fd7cf48c4c8ec81af25f742bb49edc27e5d66c3
shivaPeri <shivaperi47@gmail.com>
2022-10-27 14:24:56 -0400

    fixed sketch 10

--- sketches/sketch_010.rs
+++ sketches/sketch_010.rs
@@ -19,16 +19,17 @@ fn update(_app: &App, _model: &mut Model, _update: Update) {
 fn view(app: &App, _model: &Model, frame: Frame) {
     let draw = app.draw();
     let time = app.elapsed_frames();
-    let angle = time as f32 / 100.;
-
+    
     draw.background().color(BLACK);
-
+    
     for i in 20..100 {
-        for j in 10..500 {
-            draw.polygon()
-            .x_y((i as f32), (j as f32))
-            .points()
-            .color(WHITE);
+        for j in 10..100 {
+            let weight = (((i + j) as f32 + (time as f32)) / 100.0).sin();
+            draw.ellipse()
+                .x_y((i as f32) * 10.0 - 600.0, (j as f32) * 10.0 - 400.0)
+                .radius(((j - i) as f32) * (time as f32 / 100.0).sin())
+                .stroke(WHEAT).stroke_weight(weight)
+                .no_fill();
         }
     }
 
